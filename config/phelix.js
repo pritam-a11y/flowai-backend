35e70ca85ea12e97cffe2510e0d38d0fee994e1b
pritam-a11y <pritam@vexalink.com>
2025-10-21 18:49:11 +0530

    Update phelix.js
    fix

--- config/phelix.js
+++ config/phelix.js
@@ -1,8 +1,1 @@
-require("dotenv").config();
-
-const PHELIX_CONFIG = {
-  baseURL: process.env.PHELIX_BASE_URL,
-  access_token: process.env.PHELIX_API_KEY,
-};
-
-module.exports = PHELIX_CONFIG;
+ 
