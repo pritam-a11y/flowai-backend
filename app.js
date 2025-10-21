f17441396504262092214e939664918ea269c1dd
Pritam <pritamsamaddar840@gmail.com>
2025-10-21 17:49:19 +0530

    ADDED : Added fhelix fax-ai API's

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ const hammingRoutes = require("./routes/hamming");
 const timezoneRoutes = require("./routes/timezone");
 const membersRoutes = require("./routes/members");
 const intakeRoutes = require("./routes/intakeForm");
+const faxRoutes = require("./routes/fax");
 
 const app = express();
 
@@ -156,7 +157,8 @@ app.use("/api/v1/patient-intake-agent", patientIntakeRoutes);
 app.use("/api/v1/hamming", hammingRoutes);
 app.use("/api/v1/timezones", timezoneRoutes);
 app.use("/api/v1/members", membersRoutes);
-app.use("/api/v1/intake", intakeRoutes);
+app.use("/api/v1/intake", intakeRoutes); 
+app.use("/api/v1/fax", faxRoutes);
 
 // OAuth Routes (no prefix as per standard OAuth conventions)
 app.use("/oauth", oauthRoutes);
@@ -190,6 +192,7 @@ app.use("*", (req, res) => {
       "/api/v1/vapi/webhook",
       "/api/v1/redox/webhook/scheduling",
       "/api/v1/redox/test/trigger-scheduling-call",
+      "/api/v1/fax/upload"
     ],
   });
 });
