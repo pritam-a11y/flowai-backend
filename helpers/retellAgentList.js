3e7bbc92b897b9367d6c089530ead3834b70019f
Pritam A11Y <pritam@vexalink.com>
2025-10-24 16:39:03 +0530

    ADDED : Added find orgId by agent id

--- helpers/retellAgentList.js
+++ helpers/retellAgentList.js
@@ -45,11 +45,13 @@ async function findOrgIdByAgentId(targetAgentId) {
 
         //  Check if the target agent_id exists in the list returned by Retell
         const agents = response.data;
+        console.log("agents->", agents);
+        console.log("agents->", response.data);
 
         const matchingAgent = agents.find(
           (agent) => agent.agent_id === targetAgentId
         );
-
+        console.log("matchingAgent->", matchingAgent);
         // If a match is found, return the org_id
         if (matchingAgent) {
           logger.info("Agent ownership confirmed.", {
