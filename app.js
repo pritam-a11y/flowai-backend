93778a3bef044f36adc194cc6be88e220c1bacdb
Pritam A11Y <pritam@vexalink.com>
2025-10-30 18:44:49 +0530

    ADDED : Added list agent api

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ const membersRoutes = require("./routes/members");
 const intakeRoutes = require("./routes/intakeForm");
 const faxRoutes = require("./routes/phelix");
 const callsRoutes = require("./routes/callAnalytics");
+const listAgentRoutes = require("./routes/listAgent");
 
 const app = express();
 
@@ -161,6 +162,7 @@ app.use("/api/v1/members", membersRoutes);
 app.use("/api/v1/intake", intakeRoutes);
 app.use("/api/v1/fax", faxRoutes);
 app.use("/api/v1/callAnalytics", callsRoutes);
+app.use("/api/v1/list-agent", listAgentRoutes);
 
 // OAuth Routes (no prefix as per standard OAuth conventions)
 app.use("/oauth", oauthRoutes);
@@ -196,6 +198,7 @@ app.use("*", (req, res) => {
       "/api/v1/redox/test/trigger-scheduling-call",
       "/api/v1/fax",
       "/api/v1/callAnalytics",
+      "/api/v1/list-agent",
     ],
   });
 });
