d302a8320d8a78b9ca56bd5048618ac8fe6d0283
notsoloud <kanmohan2911@gmail.com>
2025-11-27 18:48:16 +0530

    Fix: Change patients_details to patients in updateCallStats

--- services/PatientService.js
+++ services/PatientService.js
@@ -61,7 +61,7 @@ class PatientService {
     async updateCallStats(patientId, newCallCount, newCallConfigJson) {
         logger.info("Updating patient call stats", { patientId, newCallCount });
         const query = `
-             UPDATE patients_details
+             UPDATE patients
              SET 
                  call_count = $2,
                  call_config = $3,
